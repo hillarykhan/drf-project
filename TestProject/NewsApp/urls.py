17352c443747a187735a916155686c2aed6b6d38
Hillary <khan.hillary@gmail.com>
2021-10-09 14:49:48 -0400

    adds endpoints for home and contact

--- TestProject/NewsApp/urls.py
+++ TestProject/NewsApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import News
+from .views import News, Home, Contact
 
 urlpatterns = [
-    path('newsapp/', News, name='News')
+    path('', Home, name='home'),
+    path('newsapp/', News, name='news'),
+    path('contact/', Contact, name='contact')
 ]
--- TestProject/NewsApp/views.py
+++ TestProject/NewsApp/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 
+def Home(request):
+    return HttpResponse("<h1>This is our homepage!</h1>")
+
 def News(request):
     return HttpResponse("<h1>This is our latestest news</h1>")
+
+def Contact(request):
+    return HttpResponse("<h1>This is where you can contact us!</h1>")
