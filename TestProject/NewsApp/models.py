e460ee15159bd3efeeeee60bae73808bf52bb6e4
Hillary <khan.hillary@gmail.com>
2021-10-10 20:52:16 -0400

    adds __str__ method to models

--- TestProject/NewsApp/models.py
+++ TestProject/NewsApp/models.py
@@ -7,7 +7,15 @@ class News(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
 
+
+    def __str__(self):
+        return self.author
+
 class SportNews(models.Model):
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
     description = models.TextField()
+
+
+    def __str__(self):
+        return self.author
