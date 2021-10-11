291115801dcac6f15ce3a6288d63ac1fbf93564d
Hillary <khan.hillary@gmail.com>
2021-10-11 16:51:58 -0400

    registers registrationdata model to admin panel

--- TestProject/NewsApp/admin.py
+++ TestProject/NewsApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import News, SportNews
+from .models import News, RegistrationData, SportNews
 
 # Register your models here.
 
 admin.site.register(News)
 admin.site.register(SportNews)
+admin.site.register(RegistrationData)
