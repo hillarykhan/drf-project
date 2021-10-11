5baa956ceaa5dfbaf7838f556cbb8b4d25a9779b
Hillary <khan.hillary@gmail.com>
2021-10-11 14:00:34 -0400

    adds pub_date to News model

--- TestProject/NewsApp/models.py
+++ TestProject/NewsApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class News(models.Model):
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
     description = models.TextField()
+    pub_date = models.DateField(default=timezone.now())
 
 
     def __str__(self):
